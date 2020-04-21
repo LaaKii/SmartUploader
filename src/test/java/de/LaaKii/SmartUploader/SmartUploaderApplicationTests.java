4fbffd135189c14d8b4a4c491e8624e522912c39
LaaKii <Kai-Achertal@gmx.de>
2020-04-21 13:35:11 -0700

    Project created

---
+++ src/test/java/de/LaaKii/SmartUploader/SmartUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.LaaKii.SmartUploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
